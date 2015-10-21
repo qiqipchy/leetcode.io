2c1496759e502924cad8e594b6e07aff4d45e10e
qiqipchy <980260193@qq.com>
2015-10-21 20:37:32 +0800

    Create leetcode_258_Add_Digits.java

---
+++ leetcode_258_Add_Digits.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Solution {
+	public int addDigits(int num) {
+		int sum = 0;
+		while (true) {
+			sum = 0;
+			for (; num > 0; num /= 10) {
+				sum = sum + num % 10;
+			}
+			// System.out.println("sum: " + sum + " num: " + num);
+			if (sum <= 9)
+				return sum;
+			num = sum;
+
+		}
+		// 这才是比较机智的解法！！！公式法；
+		// return (num - 1) % 9 + 1;
+	}
+
+	public static void main(String[] args) {
+		int num = 6666;
+		System.out.println(new Solution().addDigits(num));
+	}
+}
